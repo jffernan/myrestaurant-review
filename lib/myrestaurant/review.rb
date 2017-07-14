9c0d9131a39482d2539c8665bc77cbd4e9d4e650
James Fernandez <jffernan@comcast.net>
2017-07-14 15:56:02 +0000

    initialize git

---
+++ lib/myrestaurant/review.rb
@@ -0,0 +1,7 @@
+require "myrestaurant/review/version"
+
+module Myrestaurant
+  module Review
+    # Your code goes here...
+  end
+end
---
+++ lib/myrestaurant/review/version.rb
@@ -0,0 +1,5 @@
+module Myrestaurant
+  module Review
+    VERSION = "0.1.0"
+  end
+end
