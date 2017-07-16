2e88758f2b22f75d63390fea77f66a4186d66f48
James Fernandez <jffernan@comcast.net>
2017-07-16 00:50:30 +0000

    Began coding ReviewssController, copied code from my template to avoid repetitvie code

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,42 @@ class ReviewsController < ApplicationController
     erb :'reviews/reviews'
   end
 
-end
+  get '/reviews/new' do
+    erb :new
+  end
+
+  post '/reviews' do
+    @review = Review.new(:some_attribute params[:some_attribute])  #or (params[:mode])
+    @review.save
+    redirect to '/reviews/#{@review.id}'
+  end
+
+  get '/reviews' do
+     @reviews = Review.all
+     erb :'reviews/all'
+   end
+
+   get '/reviews/:id' do
+     @review = Review.find(params[:id])
+     erb :'reviews/show'
+   end 
+
+   get '/reviews/:id/edit' do
+     @review = Review.find(params[:id])
+     erb :'reviews/edit'
+   end
+
+   post '/reviews/:id' do	#PATCH use Rack::MethodOverride
+     @review = Review.find(params[:id])
+     @review.update(some_attribute: params[:some_attribute])
+     @review.save
+     redirect to '/reviews/#{@review.id}'
+   end
+
+   post '/reviews/:id/delete'#DELETE need HIDDEN button erb
+     @review = Review.find(params[:id])
+     @review.destroy
+      redirect to '/reviews/#{@review.id}'
+   end
+
+  end
